cfb9f299519faf06584746f3b2d528e9a0069144
Bardia Shafaee <shafaeebardia@gmail.com>
2026-03-26 14:06:39 -0700

    fixed bug

--- src/main/java/frc/robot/autos/SideAutoRelativeRight.java
+++ src/main/java/frc/robot/autos/SideAutoRelativeRight.java
@@ -24,6 +24,7 @@ public class SideAutoRelativeRight extends SequentialCommandGroup {
         double MaxAngRate
     ) {
         addCommands(
+            Commands.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.fromDegrees(180))),
             drivetrain.applyRequest(() -> drive.withRotationalRate(0.3 * MaxAngRate))
                 // .until(() -> vision.isAnyAllianceHubAnySide())
                 .withTimeout(0.75),
@@ -31,11 +32,10 @@ public class SideAutoRelativeRight extends SequentialCommandGroup {
             drivetrain.applyRequest(() -> drive.withRotationalRate(0)).withTimeout(0.1),
             Commands.run(() -> vision.faceAprilTag(drivetrain, drive, brake, MaxAngRate), vision, drivetrain)
                 .until(() -> vision.isFacingAprilTag())
-                .onlyIf(() -> vision.isAprilTag())
                 .finallyDo(() -> drivetrain.setControl(brake))
                 .withTimeout(0.5),
             Commands.parallel(
-                Commands.run(() -> fuel.runUp(fuel.calcSpeedByDistance(vision.getZ()) + 0.04), fuel, vision).withTimeout(0.2),
+                Commands.run(() -> fuel.runUp(fuel.calcSpeedByDistance(vision.getZ())), fuel, vision).withTimeout(0.2),
                 Commands.runOnce(() -> indexer.startHopperIntake(), indexer),
                 Commands.waitSeconds(1.5)
             ),
